6671341a6ece3d7573c44e33a912c0c77062709b
Rahini02024 <rahinirajasekaran2004@gmail.com>
2026-09-15 14:41:44 +0530

    Add files via upload

---
+++ Loginsteps.java
@@ -0,0 +1,32 @@
+package loginsteps;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+import io.cucumber.java.en.Then;
+
+public class Loginsteps {
+
+@Given("User is on the login page")
+public void user_is_on_the_login_page() {
+    System.out.println("User is on login page");
+}
+
+@When("User enters valid username and password")
+public void user_enters_valid_username_and_password() {
+    System.out.println("User enters valid username and password");
+}
+
+@When("User clicks the Login button")
+public void user_clicks_the_login_button() {
+    System.out.println("User clicks the Login button");
+}
+
+@Then("User should be logged in successfully")
+public void user_should_be_logged_in_successfully() {
+    System.out.println("User should be logged in successfully");
+}
+}
+
+
+
+
